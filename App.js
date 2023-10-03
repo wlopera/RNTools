1ac6b8cf003c00e341fba84211df4af9a86eaf8e
William Lopera <lopera.william@gmail.com>
2023-10-03 16:59:05 -0500

    Create README.md

--- App.js
+++ App.js
@@ -4,17 +4,45 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import AllPlace from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlace} />
-          <Stack.Screen name="AppPlaces" component={AddPlace} />
+        <Stack.Navigator screenOptions={{
+          headerStyle: {backgroundColor: Colors.primary500},
+          headerTintColor: Colors.gray700,
+          contentStyle: {backgroundColor: Colors.gray700},
+        }}>
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlace}
+            options={({ navigation }) => ({
+              title: "Lugares Favoritos",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => {
+                    navigation.navigate("AppPlace");
+                  }}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AppPlace"
+            component={AddPlace}
+            options={{
+              title: "Agregar un Lugar",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,11 +1,47 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { TextInput, ScrollView, View, Text, StyleSheet } from "react-native";
+import {Colors} from '../../constants/colors'
 
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredtext) {
+    setEnteredTitle(enteredtext);
+  }
+
   return (
-    <View>
-      <Text>Formulario de Lugares</Text>
-    </View>
+    <ScrollView  style={styles.form}>
+      <View>
+        <Text style={styles.label}>Titulo</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 2,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100
+  },
+});
